6bb7ea01552016c518c2fd06fc87448808c0cbb1
Pablo Molina <molinapablod@gmail.com>
2024-05-21 14:14:01 +1000

    compoenents and intro screen

--- app/src/main/java/com/pdm/runique/MainActivity.kt
+++ app/src/main/java/com/pdm/runique/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pdm.runique
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,37 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.pdm.runique.auth.presentation.intro.IntroScreenRoot
 import com.pdm.runique.core.presentation.designsystem.RuniqueTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RuniqueTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    IntroScreenRoot({}, {})
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RuniqueTheme {
-        Greeting("Android")
     }
 }
