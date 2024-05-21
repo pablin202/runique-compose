0ac0106051e9ff7ae59f7ecdf757e5260f81897b
Pablo Molina <molinapablod@gmail.com>
2024-05-21 22:06:51 +1000

    splash screen

--- app/src/main/java/com/pdm/runique/MainActivity.kt
+++ app/src/main/java/com/pdm/runique/MainActivity.kt
@@ -8,19 +8,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.pdm.runique.core.presentation.designsystem.RuniqueTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.state.isCheckingAuth
+            }
+        }
         setContent {
             RuniqueTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
-                    val navController = rememberNavController()
-                    NavigationRoot(navController = navController)
+                    if (!viewModel.state.isCheckingAuth) {
+                        val navController = rememberNavController()
+                        NavigationRoot(
+                            navController = navController,
+                            isLoggedIn = viewModel.state.isLoggedIn
+                        )
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/pdm/runique/MainState.kt
@@ -0,0 +1,6 @@
+package com.pdm.runique
+
+data class MainState(
+    val isLoggedIn: Boolean = false,
+    val isCheckingAuth: Boolean = false,
+)
