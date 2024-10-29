c12236cb6dae292f72c75ce853310055d2526b30
Pablo Molina <molinapablod@gmail.com>
2024-10-29 16:28:43 +1100

    saving pending runs

--- core/database/src/main/java/com/pdm/runique/core/database/RunDatabase.kt
+++ core/database/src/main/java/com/pdm/runique/core/database/RunDatabase.kt
@@ -3,13 +3,17 @@ package com.pdm.runique.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.pdm.runique.core.database.dao.RunDao
+import com.pdm.runique.core.database.dao.RunPendingSyncDao
 import com.pdm.runique.core.database.entity.RunEntity
+import com.pdm.runique.core.database.entity.RunPendingSyncEntity
+import com.pdm.runique.core.database.entity.DeletedRunSyncEntity
 
 @Database(
-    entities = [RunEntity::class],
+    entities = [RunEntity::class, RunPendingSyncEntity::class,
+        DeletedRunSyncEntity::class],
     version = 1
 )
-abstract class RunDatabase: RoomDatabase() {
-
+abstract class RunDatabase : RoomDatabase() {
+    abstract val runPendingSyncDao: RunPendingSyncDao
     abstract val runDao: RunDao
 }
